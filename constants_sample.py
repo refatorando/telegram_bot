099535bfec9265293ae0a2d2e7299c26a7a6e5e9
Roger Santos <rogersantos.cwb@gmail.com>
2021-03-09 21:25:04 -0600

    v0

--- constants_sample.py
+++ constants_sample.py
@@ -1,4 +1,4 @@
-API_KEY = "api key, fornecida pelo bot"
-API_ID = 'api id fornecida ao criar o app'
-API_HASH = 'api hash fornecida ao criar o app'
+API_KEY = "api key, fornecida pelo bot "
+API_ID = 'api id fornecida ao criar o app https://my.telegram.org/apps'
+API_HASH = 'api hash fornecida ao criar o app https://my.telegram.org/apps'
 PHONE = 'numero de telefone para receber confirmação e enviar a mensagem com +55xxxxxxxxx'
